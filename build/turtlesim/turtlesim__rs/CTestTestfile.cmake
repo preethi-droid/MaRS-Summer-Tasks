03cfa5f325d5f3180c653dfe33ad1f2649f8df31
preethi-droid <preethiabhiramik@gmail.com>
2026-05-18 18:46:41 +0530

    Initial commit

---
+++ build/turtlesim/turtlesim__rs/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/preethi/ros2_ws/build/turtlesim/turtlesim__rs
+# Build directory: /home/preethi/ros2_ws/build/turtlesim/turtlesim__rs
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
